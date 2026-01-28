bd1d05ad5ce8c5a806fb0b7f1879bfbdce2b8d95
Derlis Nunhez <danynu015@gmail.com>
2026-01-28 15:11:11 -0300

    Se agregan comentarios para comentar las funciones del codigo

--- wordle.py
+++ wordle.py
@@ -6,22 +6,24 @@ palabra_correcta = list(palabra)
 print('Bienvenido a Wordle Python Edition')
 print('Tiene 6 intentos\nAdivine la palabra')
 print(f'La palabra a adivinar contiene {len(palabra_correcta)} letras\n')
+print(f'Tambien debe utilizar palabras de {len(palabra_correcta)} para jugar\n')
 
 #Se abre una funcion, en este caso es solo para tener ordenada la presentacion, logica del juego y el mensaje final
 def verificador_palabra(palabra_correcta, intentos):
     #Definimos las veces que va a jugar con la variable 'intento', que en este caso estaba definida
     for turno in range(intentos):
         letra = input('Por favor ingrese la palabra\n').lower()
-        #Establecemos una condicion que avise al usuario cuantas letras deberia tener la palabra ingresada
+        #Establecemos una condicion que avise al usuario cuantas letras deberia tener la palabra ingresada y se procede a cerrar el juego
         if len(letra) < len(palabra_correcta) or len(letra) > len(palabra_correcta):
             print(f'La palabra debe tener {len(palabra_correcta)} letras\n')
             break
+        #Agrega las letras de la palabra ingresada a una lista
         palabras = list(letra)
 
         if palabras == palabra_correcta:
             print('Felicidades, ganaste el juego')
             return 
-
+        #Desglosamos la palabra correcta y comparamos letra por letra la palabra ingresada
         for i in range(len(palabra_correcta)):
             if palabras[i] == palabra_correcta[i]:
                 letras_verificadas.append(f"[{palabras[i]}]")
